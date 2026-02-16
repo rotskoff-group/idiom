9e2b9ef0e68cb122671a865d6415dd2e959d7015
Jason Liu <jsliu9@gmail.com>
2026-02-16 11:09:54 -0800

    Move input/target_generators.py to subdir

---
+++ src/idr_plm/nn/transformer/generators/__init__.py
@@ -0,0 +1,9 @@
+from .input_generators import InputGeneratorBase, SMILESInputBasic
+from .target_generators import TargetGeneratorBase, SMILESTarget
+
+__all__ = [
+    "InputGeneratorBase",
+    "SMILESInputBasic",
+    "TargetGeneratorBase",
+    "SMILESTarget",
+]
