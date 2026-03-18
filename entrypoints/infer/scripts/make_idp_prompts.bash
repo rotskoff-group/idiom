1a96a083e211acf996c14533945afbb25319f73e
Jason Liu <jsliu9@gmail.com>
2026-03-18 14:54:00 -0700

    Add CLI command and bash scripts for 'make_prompt' for making IDP or IDR prompts

---
+++ entrypoints/infer/scripts/make_idp_prompts.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
+
+# Make prompts for generating IDPs
+transformer_make_prompt \
+    --shard   "${REPO_ROOT}/models/data/shard/0001_file.h5" \
+    --out_dir "${REPO_ROOT}/models/data/prompts" \
+    idp \
+    --num_duplicates 10000
