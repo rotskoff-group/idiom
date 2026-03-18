4cdab7ca4be351d5a44599238b0f7deb3cbb1197
Jason Liu <jsliu9@gmail.com>
2026-03-18 16:23:53 -0700

    Rename make_prompt to make_infer_prompt.

--- entrypoints/infer/scripts/make_idp_prompts.bash
+++ entrypoints/infer/scripts/make_idp_prompts.bash
@@ -3,7 +3,7 @@
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 
 # Make prompts for generating IDPs
-transformer_make_prompt \
+transformer_make_infer_prompt \
     --shard   "${REPO_ROOT}/models/data/shard/0001_file.h5" \
     --out_dir "${REPO_ROOT}/models/data/prompts" \
     idp \
