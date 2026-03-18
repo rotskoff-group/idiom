8b04faca291ad1fd4328735c06bf8502413ab054
Jason Liu <jsliu9@gmail.com>
2026-03-18 09:36:46 -0700

    Change imports to absolute in target_ and input_generators.py

--- src/idr_plm/nn/layers/__init__.py
+++ src/idr_plm/nn/layers/__init__.py
@@ -1,14 +1,5 @@
-from .blocks import UnifiedTransformerBlock
-from .mha import MultiHeadAttention
-
-# from .geometric_attention import GeometricAttention
-from .transformer_stack import TransformerStack
-from .rpe import RelativePositionEmbedding
-# from .codebook import EMACodebook, MolCodebook
-
-# from .structure_proj import Dim6RotStructureHead
-# from .ppe import PairwisePredictionHead
-from .regression_head import RegressionHead
-# from .structure_token import StructureTokenEncoder, StructureTokenDecoder
-# from .transfusion_embedding import TransfusionEmbedding
-# from .diffusion import TorDiffLoss, DDPMLoss
+from .blocks import UnifiedTransformerBlock as UnifiedTransformerBlock
+from .mha import MultiHeadAttention as MultiHeadAttention
+from .transformer_stack import TransformerStack as TransformerStack
+from .rpe import RelativePositionEmbedding as RelativePositionEmbedding
+from .regression_head import RegressionHead as RegressionHead
